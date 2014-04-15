e9f27f95cb129d4adc5999180f53386de5ff979c
Hugo Duncan <hugo@palletops.com>
2014-04-15 14:20:43 +0000

    Updated version for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.palletops/pallet-aws-ops "0.2.0"
+(defproject com.palletops/pallet-aws-ops "0.2.1-SNAPSHOT"
   :description "Basic operations for building on aws"
   :url "https://github.com/pallet/pallet-aws-ops"
   :license {:name "Eclipse Public License"
