fea8474424240474227db7c49dbfd28bc41a21c7
Hugo Duncan <hugo@hugoduncan.org>
2014-09-30 12:59:19 -0400

    Update to awaze 0.1.2

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [com.palletops/awaze "0.1.1"
+                 [com.palletops/awaze "0.1.2"
                   :exclusions [commons-logging commons-codec]]
                  [commons-codec "1.6"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
